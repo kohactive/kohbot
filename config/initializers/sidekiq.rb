f914eb8a1e27b15649caed43a29f8ed9af12a504
Laura Kajpust <lkajpust@gmail.com>
2018-12-28 13:24:59 -0600

    Sidekiq updates

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,4 @@
-Sidekiq.configure_server do |config|
-  schedule_file = Rails.root.join('config', 'schedule.yml')
-
-  if File.exists?(schedule_file)
-    Sidekiq::Cron::Job.load_from_hash YAML.load_file(schedule_file)
-  end
+schedule_file = Rails.root.join('config', 'schedule.yml')
+if File.exists?(schedule_file)
+  Sidekiq::Cron::Job.load_from_hash YAML.load_file(schedule_file)
 end
