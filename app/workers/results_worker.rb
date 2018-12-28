432a552db029680adf5a922b228a04295e7028b5
Laura Kajpust <lkajpust@gmail.com>
2018-12-28 14:15:05 -0600

    Update code to actually submit to #general

--- app/workers/results_worker.rb
+++ app/workers/results_worker.rb
@@ -5,11 +5,13 @@ class ResultsWorker
   def perform
     # Check there is an open question
     # We don't need to do anything if not
+    logger.info("Starting results worker")
     if Question.open.any?
       question = Question.open.first
       message = ":rocket: *The results are in!* :rocket:\n"
       attachments = []
       # Did we get responses?
+      logger.info("Checking for responses on question #{question.id}")
       if question.responses.none?
         attachments.push({
               fallback: "...Or not? *Nobody submitted their answers!* :flushed: How embarassing.",
@@ -19,6 +21,7 @@ class ResultsWorker
         })
       else
         # Yes -- publish responses
+        logger.info("Got responses for #{question.id}")
         question.responses.each do |answer|
           attachments.push({
             fallback: "#{answer.answer}",
@@ -27,11 +30,14 @@ class ResultsWorker
             text: "#{answer.answer}"
           })
         end
-        channel = "RESULTS"
-        PostToSlack.post_slack_msg( channel, message, attachments )
       end
+      logger.info("Attempting to post.")
+      channel = "RESULTS"
+      PostToSlack.post_slack_msg( channel, message, attachments )
       # Set question to inactive
       question.mark_as_closed!
+    else
+      logger.info("No active question to post results for.")
     end # end check if question
   end
 end
--- lib/post_to_slack.rb
+++ lib/post_to_slack.rb
@@ -3,11 +3,16 @@ class PostToSlack
   def self.post_slack_msg( channel, message, attachments )
     token = ENV["BOT_AUTH"]
     begin
-      attachments = attachments.to_json
       if channel === "RESULTS"
-        options = { query: { text: message, attachments: attachments } }
-        url = ENV["RESULTS_CHANNEL"]
+        Rails.logger.info("Posting to Results Channel")
+        options = {
+          :body => { :text => message, :attachments => attachments }.to_json,
+          :headers => { 'Content-Type' => 'application/json', 'Accept' => 'application/json' }
+        }
+        url = ENV["RESULTS_HOOK"]
       else
+        attachments = attachments.to_json
+        Rails.logger.info("Preparing to post a DM with KohBot")
         options = { query: { channel: channel, text: message, attachments: attachments }, headers: { 'Authorization' => "Bearer #{token}"} }
         url = 'https://slack.com/api/chat.postMessage'
       end
